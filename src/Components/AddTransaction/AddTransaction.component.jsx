fac918ad3b12822e2622786c91d583807a0b7c13
Nick Morgan <nick@social-sync.com>
2020-11-17 18:38:48 +0000

    Fixed issues.

---
+++ src/Components/AddTransaction/AddTransaction.component.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from "../../context/GlobalState";
+
+const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+    
+    const { addTransaction } = useContext(GlobalContext);
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        
+        const newTransaction = {
+            id: Math.floor(Math.random() * 1000000000),
+            text,
+            amount: +amount
+        }
+        
+        addTransaction(newTransaction);
+        setText('');
+        setAmount('');
+    }
+    
+    return (
+        <div>
+            <h3>Add new transaction</h3>
+            <form id="form" onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <label htmlFor="text">Text</label>
+                    <input type="text" id="text" placeholder="Enter text..." value={text} onChange={(e) => setText(e.target.value)}/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="amount">Amount <br/>
+                        (negative - expense, positive - income)</label>
+                    <input type="number" id="amount" placeholder="Enter amount..." value={amount} onChange={(e) => setAmount(e.target.value)} inputMode='numeric'/>
+                </div>
+                <button className="btn">Add transaction</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddTransaction;
